cae44c1001da51c47a56176436d822dd05c0ac10
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-07-23 12:40:55 +0900

    데이터 전처리 완료

---
+++ Data/DataPrepration.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+
+# 정신건강 FAQ 데이터셋
+faq_df = pd.read_csv('Mental_Health_FAQ.csv')
+
+print(faq_df.head())
+
+# 정신건강 데이터셋
+with open('Mental_Health_Conversations.json', 'r', encoding='utf-8') as file:
+    conversation_data = json.load(file)
+
+# patterns와 responses를 추출하여 데이터프레임으로 변환
+data = []
+
+for intent in conversation_data['intents']:
+    if 'responses' in intent:
+        for pattern in intent['patterns']:
+            for response in intent['responses']:
+                data.append({'pattern': pattern, 'response': response})
+    else:
+        for pattern in intent['patterns']:
+            data.append({'pattern': pattern, 'response': ''})
+
+conversation_df = pd.DataFrame(data)
+
+# 대화 데이터 전처리
+conversation_df['pattern'] = conversation_df['pattern'].str.lower()
+conversation_df['response'] = conversation_df['response'].str.lower()
+
+print(conversation_df.head())
+
+
+# 감정분석 데이터 로드
+sentiment_df = pd.read_csv('sentiment_analysis_data.csv')
+
+sentiment_df['statement'] = sentiment_df['statement'].str.lower()
+sentiment_df['status'] = sentiment_df['status'].str.lower()
+
+print(sentiment_df.head())
+
+# 우울증 감지 데이터 로드
+depression_df = pd.read_csv('Depression_Detection.csv')
+
+print(depression_df.head())
